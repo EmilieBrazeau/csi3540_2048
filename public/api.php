58d0e87ec3b309b00beabb72615dfacb5efc643a
Émilie Brazeau <volleyball.emilie.brazeau@gmail.com>
2023-03-07 13:46:21 -0500

    Lab0 7: api.php

---
+++ public/api.php
@@ -0,0 +1,17 @@
+<?php
+require_once('_config.php');
+
+$path = $_GET["action"] ?: "/2048/404";
+
+switch ($path) {
+case "/2048/new":
+  $game = new Game2048();
+  $json = $game->toEncodedJson();
+  break;
+
+default:
+  $json = json_encode(["error" => "Unknown path {$path}"]);
+}
+
+header("Content-Type: application/json");
+echo $json;
